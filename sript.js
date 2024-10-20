a1fbaa29e82f418bdf320b6617a05dc4af13e844
Adesh singh <155350029+adeshsinghbits@users.noreply.github.com>
2024-10-20 19:17:30 +0530

    Add files via upload

---
+++ sript.js
@@ -0,0 +1,5 @@
+// This is a function for changing background color of whole body
+const colorChanger = (color) => {
+        let body = document.querySelector('.body')
+        body.style.backgroundColor = `${color}`;
+}
